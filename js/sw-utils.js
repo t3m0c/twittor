d7339b4182bcf6f0d10dc3fbadfbb6dc09d83439
Cuauhtemoc Olivares Pizano <cuauhtemoc.olivares@ksp.com.mx>
2025-04-10 20:05:34 -0600

    Modificacmos la ruta del SW 5

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,4 +1,4 @@
-function actualizaCacheDinamico( dynamicCache, req, res ) {
+function actualizarCacheDinamico( dynamicCache, req, res ) {
   if ( res.ok ) {
     return caches.open( dynamicCache ).then( cache => {
       cache.put( req, res.clone() );
